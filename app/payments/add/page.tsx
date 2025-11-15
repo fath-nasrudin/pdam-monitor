88e3a5573a47a0b694f5c593906c005a0f5bd9f4
fath-nasrudin <fath.nasrudin.i@gmail.com>
2025-11-15 13:03:32 +0700

    Add create payment page ui

---
+++ app/payments/add/page.tsx
@@ -0,0 +1,13 @@
+import { Card, CardHeader, CardTitle } from "@/components/ui/card";
+
+export default function AddPaymentPage() {
+  return (
+    <div className="min-h-screen">
+      <Card>
+        <CardHeader>
+          <CardTitle>Tambah Pembayaran</CardTitle>
+        </CardHeader>
+      </Card>
+    </div>
+  );
+}
