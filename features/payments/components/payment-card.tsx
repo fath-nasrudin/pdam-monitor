36ac6c05d680edbbd00d6cdb675231970a6c0c57
fath-nasrudin <fath.nasrudin.i@gmail.com>
2025-11-24 16:03:43 +0700

    Add simple payment card component

---
+++ features/payments/components/payment-card.tsx
@@ -0,0 +1,11 @@
+import { Payment } from "../payment.type";
+
+export function PaymentCard({ payment }: { payment: Payment }) {
+  return (
+    <div className="flex flex-col gap-2 justify-between border p-1">
+      <div>{payment.userId}</div>
+      <div>{payment.amount}</div>
+      <div>{payment.method}</div>
+    </div>
+  );
+}
