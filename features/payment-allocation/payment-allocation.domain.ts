088d5018c80e652570b03408180d3404550b7a4f
fath-nasrudin <fath.nasrudin.i@gmail.com>
2025-11-27 11:47:12 +0700

    Add payment allocation business logic

--- features/payment-allocation/payment-allocation.domain.ts
+++ features/payment-allocation/payment-allocation.domain.ts
@@ -1,15 +1,28 @@
+import { Invoice } from "../invoice/invoice.type";
+import { Payment } from "../payments/payment.type";
 import { CreatePaymentAllocationDomainInput } from "./payment-allocation.repo";
 import { CreatePaymentAllocationSchemaInput } from "./payment-allocation.schema";
 
 export function createPaymentAllocation({
+  invoice,
+  payment,
   paymentAllocation,
 }: {
   paymentAllocation: CreatePaymentAllocationSchemaInput;
+  invoice: Invoice;
+  payment: Payment;
 }): CreatePaymentAllocationDomainInput {
-  // add business logic later
-  // throw error if the payAlloc amount > payment.remaining
-  // throw error if the payAlloc amount > inv.notalreadypaid
-  // throw error if payAlloc amount <= 0
+  if (paymentAllocation.amount <= 0) {
+    throw new Error("PAYMENT_ALLOCATION_MUST_BE_POSITIVE_NUMBER");
+  }
+
+  if (payment.amountRemaining < paymentAllocation.amount) {
+    throw new Error("INSUFFICIENCE_PAYMENT_AMOUNT_REMAINING");
+  }
+  const invoiceTotalUnpaid = invoice.totalAmount - invoice.totalPaid;
+  if (invoiceTotalUnpaid < paymentAllocation.amount) {
+    throw new Error("TOTAL_PAYMENT_ALLOCATION_BIGGER_THAN_TOBEPAID");
+  }
 
   return paymentAllocation;
 }
--- features/payment-allocation/payment-allocation.service.ts
+++ features/payment-allocation/payment-allocation.service.ts
@@ -2,18 +2,25 @@ import { CreatePaymentAllocationSchemaInput } from "./payment-allocation.schema"
 import * as PayAllocDomain from "@/features/payment-allocation/payment-allocation.domain";
 import * as PayAllocRepo from "@/features/payment-allocation/payment-allocation.repo";
 import { PaymentAllocation } from "./payment-allocation.type";
+import { getPaymentById } from "../payments/payment.service";
+import { getInvoiceById } from "../invoice/invoice.service";
 
 export async function createPaymentAllocation(
   data: CreatePaymentAllocationSchemaInput
 ): Promise<PaymentAllocation> {
   // should take the invoice and payment
   // feed to create pay alloc domain to check some business rule
+  const payment = await getPaymentById(data.paymentId);
+  const invoice = await getInvoiceById(data.invoiceId);
 
   // if all good
   // should update the payment status of invoice and payment
   // should create the payAlloc
+
   const payAlloc = PayAllocDomain.createPaymentAllocation({
     paymentAllocation: data,
+    invoice,
+    payment,
   });
 
   return PayAllocRepo.createPaymentAllocation(payAlloc);
