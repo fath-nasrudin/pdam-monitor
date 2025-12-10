e2319129f1194948a6a6f944c3a7e66241016f70
fath-nasrudin <fath.nasrudin.i@gmail.com>
2025-12-10 17:47:37 +0700

    Add generate billingPeriod utils

--- lib/utils.ts
+++ lib/utils.ts
@@ -15,3 +15,40 @@ export function formatRupiah(value: number | string) {
     minimumFractionDigits: 0,
   }).format(num);
 }
+
+export function toBillingPeriodYYYYMM(date: Date | number | string) {
+  const d =
+    typeof date === "number"
+      ? new Date(date)
+      : typeof date === "string"
+      ? new Date(date)
+      : date;
+
+  const year = d.getFullYear();
+  const month = String(d.getMonth() + 1).padStart(2, "0");
+
+  return `${year}-${month}`;
+}
+
+export function getPrevBillingPeriod(
+  date: Date | number | string = new Date()
+) {
+  const d =
+    typeof date === "number"
+      ? new Date(date)
+      : typeof date === "string"
+      ? new Date(date)
+      : date;
+
+  let year = d.getFullYear();
+  let month = d.getMonth() + 1; // 1–12
+
+  month -= 1; // mundur satu bulan
+
+  if (month === 0) {
+    month = 12;
+    year -= 1;
+  }
+
+  return `${year}-${String(month).padStart(2, "0")}`;
+}
